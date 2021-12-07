0812d5deed39739510266d14b20a5882715643bc
seymourpublic <naledisandamela@gmail.com>
2021-12-07 13:55:41 +0200

    Add stars

--- CV-Project2/main.js
+++ CV-Project2/main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 import { Color } from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000);
@@ -26,8 +28,23 @@ pointLight.position.set(5,5,5)
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight,ambientLight)
 
-const lightHelper = new THREE.PointLightHelper(pointlight)
-scene.add(lightHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,5);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+   const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+   const star = new THREE.Mesh(geometry, material);
+
+   const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+   star.position.set(x,y,z);
+   scene.add(star)
+}
+
+Array(450).fill().forEach(addStar)
 
 function animate()
 {
@@ -35,10 +52,10 @@ function animate()
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
   
   renderer.render(scene, camera);
-
-
 }
 
 animate()
